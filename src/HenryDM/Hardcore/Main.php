0300dca8d8fa837fdab79dbf86adf88c5f3f0b31
HenryDM <100979680+Henry12960@users.noreply.github.com>
2022-09-26 14:27:05 -0300

    Create Main.php

---
+++ src/HenryDM/Hardcore/Main.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace HenryDM\Harcore;
+
+# =======================
+#    Pocketmine Class
+# =======================
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase implements Listener {
+
+}
